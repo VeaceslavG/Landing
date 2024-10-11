b9b72e2f6ef4728844deda671a5e5e8bf5385fb3
veaceslav <p.test.email101@gmail.com>
2024-10-11 22:39:43 +0300

    Changed err file

---
+++ script/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const fshBtn = document.querySelector('.closeFshBtn');
+
+fshBtn.addEventListener('click', (event) => {
+  document.querySelector(".fsh").style = "display:none";
+});
